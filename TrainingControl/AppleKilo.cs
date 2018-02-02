ed6bc83a1ce36f7a268de487444bbebe2c73d46b
UkiOnna <sikirovtemirlan@mail.ru>
2018-02-02 19:01:10 +0600

    fixed

---
+++ TrainingControl/AppleKilo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingControl
+{
+    public class AppleKilo: INotifyPropertyChanged
+    {
+        public Guid Id { get; set; }
+        public string City { get; set; }
+        public double ApplePrice { get { return applePrice; } set { applePrice = value; NotifyPropertyChanged(); }  } 
+        public double GrowPrice { get; set; } = 15;
+        public double Loss { get; set; }
+        public double DeliveryPrice { get; set; } = 10;
+        public double Profit { get { return profit; } set { profit = value;NotifyPropertyChanged(); } }
+
+        private double applePrice = 0;
+        private double profit = 0;
+        public AppleKilo()
+        {
+            Id = new Guid();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public double CountLoss()
+        {
+            Loss = GrowPrice + DeliveryPrice;
+            return Loss;
+        }
+
+        public double CountProfit()
+        {
+            Profit = ApplePrice - Loss;
+            return Profit;
+        }
+
+    }
+}
---
+++ TrainingControl/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace TrainingControl
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<AppleKilo> apples;
+        private XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<AppleKilo>));
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            apples = new ObservableCollection<AppleKilo>
+            {
+            new AppleKilo { Id=new Guid(),  ApplePrice = 0, City = "Астана", DeliveryPrice = 10, GrowPrice = 10 },
+            new AppleKilo {Id=new Guid(), ApplePrice = 0, City = "Уральск", DeliveryPrice = 20, GrowPrice = 30 },
+            new AppleKilo { Id=new Guid(),ApplePrice = 0, City = "Иркутск", DeliveryPrice = 50, GrowPrice = 10 }
+            };
+            //using (FileStream stream = new FileStream(Directory.GetCurrentDirectory() + @"\data.xml", FileMode.OpenOrCreate))
+            //{
+            //    apples = serializer.Deserialize(stream) as ObservableCollection<AppleKilo>;
+            //}
+            AppleContainer.ItemsSource = apples;
+            AppleContainer.CellEditEnding += new EventHandler<DataGridCellEditEndingEventArgs>(AppleContainerCellEditEnding);
+           // AppleContainer.TouchEnter += new EventHandler<TouchEventArgs>(AppleContainer_TouchEnter);
+        }
+
+
+
+        private void AppleContainerCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            //IList rows = AppleContainer.SelectedItems;
+            //DataRowView row = (DataRowView)AppleContainer.SelectedItems[0];
+            //MessageBox.Show(  row[1].ToString());
+         
+
+
+            using (FileStream stream = new FileStream(Directory.GetCurrentDirectory() + @"\data.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(stream, apples);
+            }
+        }
+
+        private void AppleContainerCurrentCellChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < apples.Count; i++)
+            {
+
+                if (apples[i].ApplePrice != 0 && apples[i].ApplePrice != null)
+                {
+                    apples[i].CountLoss();
+                    apples[i].CountProfit();
+                }
+                
+
+            }
+        }
+
+        private void AppleContainer_TouchEnter(object sender, TouchEventArgs e)
+        {
+          
+        }
+    }
+}
